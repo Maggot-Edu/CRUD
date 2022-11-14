cc8ec7c0b59f45b80011177b6ded5e76189b3cf0
Eduard <eduardbadoyan@gmail.com>
2022-11-14 18:21:53 +0100

    Creacion head y footer

--- view/head/head.php
+++ view/head/head.php
@@ -1,3 +1,6 @@
+<?php 
+    
+?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -49,3 +52,26 @@
                 </div>
             </div>
         </div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Pilotos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Iniciar sesión</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Registrarse</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
