3f3c05efb9a1dcc88bd5c24c506aa0f4baa39777
Eduard <eduardbadoyan@gmail.com>
2022-11-14 18:06:04 +0100

    bootstrap+jquery+datatables+popper

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
+    require_once "view/head/head.php";
+?>
 
-
-class conexion {
-    
-    private $host = 'localhost';
-    private $dbname = 'f1';
-    private $user = 'root';
-    private $password = '';
-    public function conexion() {
-        try {
-            $PDO = new PDO("mysql:host=".$this->host."; dbname=".$this->dbname, $this->user , $this->password);
-            return $PDO;
-        } catch (PDOException $e) {
-            return $e->getMessage();
-        }
-    }
-}
-
-$obj = new conexion();
-print_r($obj->conexion());
+<?php
+    require_once "view/head/footer.php";
+?>
